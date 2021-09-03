309e11e1f726b0229d39f6deaf78c6576512f1c6
Gabriel Lacerda <devlacerda1109@gmail.com>
2021-09-03 11:41:16 -0300

    Project start

---
+++ js/setup.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        400:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:8
+        }
+    }
+})
